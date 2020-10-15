0c467c5a7cda24af543829b9f56fd5d2f8e1f466
Brenda Gutiérrez <blilianagr@gmail.com>
2020-10-14 17:39:52 -0700

    Second exercise

---
+++ variables.py
@@ -0,0 +1,34 @@
+
+##Declaracion de variables
+
+##String
+text="Variable string"
+
+print(text)
+
+##Int, Float
+num1=int(34)
+
+print(num1)
+
+num2=float(24.5)
+
+print(num2)
+
+
+##Boolean
+bol1=bool(1)
+bol2=bool(0)
+
+print("La siguiente variable es: ", bol1)
+print("La siguiente variable es: ", bol2)
+
+
+##Array
+lista=[1,2,3,4,5,6,7,8,9,10]
+
+print(lista)
+
+lista2=["a","b","c","d","e","f","g"]
+
+print(lista2)
