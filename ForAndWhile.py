1f89d120e8d8890b021727ba80518a4b44d7c512
Brenda Gutiérrez <blilianagr@gmail.com>
2020-10-18 23:32:59 -0600

    Update of the fourth exercise

--- ForAndWhile.py
+++ ForAndWhile.py
@@ -2,9 +2,23 @@
 
 ## 1)
 
+for i in range (25,36):
+	print (i)
+
+print('')
+print('')
+
 for i in range(101):
 	print(i)
 
+print('')
+print('')
+
+for i in range (20,41):
+	print(i)
+
+print('')
+print('')
 
 ## 2)
 
@@ -13,3 +27,14 @@ i = 1
 while i < 101:
 	print(i)
 	i+=1
+
+print('')
+print('')
+
+e = 2 
+
+while e < 101:
+	print(e)
+	e+=2
+print('')
+print('')
