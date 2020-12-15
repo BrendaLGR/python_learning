4da80accc9daccc346c2f65ccca63c2519e7cb2e
Brenda Gutiérrez <blilianagr@gmail.com>
2020-12-15 01:26:32 -0700

    Space is removed

--- SumOfPairs.py
+++ SumOfPairs.py
@@ -5,7 +5,7 @@ suma = 0
 n1 = int(input('Numero inicial: '))
 n2 = int(input('Numero final: '))
 
-for i in range (n1,n2+1):
+for i in range(n1,n2+1):
 	if i % 2 == 0:
 		suma += i
 print('\n La suma es: ', suma)
