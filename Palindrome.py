07700a9e1afe42378441a1bd8e9dbd39017fe13f
Brenda Gutiérrez <blilianagr@gmail.com>
2021-01-28 10:41:32 -0700

    Palindrome exercise

---
+++ Palindrome.py
@@ -0,0 +1,18 @@
+#Crear un script que pasado una cadena como argumento diga si dicha cadena es palindrome
+
+
+igual, aux = 0, 0
+
+texto = input("Ingrese la palabra que desea evaluar: ")
+
+for ind in reversed(range(0, len(texto))):
+  if texto[ind].lower() == texto[aux].lower():
+    igual += 1
+  aux += 1
+
+if len(texto) == igual:
+  print("El texto es palindrome")
+else:
+  print("El texto no es palindrome")
+
+
